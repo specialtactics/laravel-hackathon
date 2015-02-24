7c19703420cd18f385444f4a2f4187276720283f
Tom Somerville <tom.somerville@4mation.com.au>
2015-02-24 19:03:12 +1100

    Flysystem config

---
+++ config/flysystem.php
@@ -0,0 +1,94 @@
+<?php
+
+/*
+ * This file is part of Laravel Flysystem.
+ *
+ * (c) Graham Campbell <graham@mineuk.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Default Connection Name
+    |--------------------------------------------------------------------------
+    |
+    | Here you may specify which of the connections below you wish to use as
+    | your default connection for all work. Of course, you may use many
+    | connections at once using the manager class.
+    |
+    */
+
+    'default' => 'local',
+
+    /*
+    |--------------------------------------------------------------------------
+    | Flysystem Connections
+    |--------------------------------------------------------------------------
+    |
+    | Here are each of the connections setup for your application. Examples of
+    | configuring each supported driver is shown below. You can of course have
+    | multiple connections per driver.
+    |
+    */
+
+    'connections' => [
+
+        'local' => [
+            'driver'     => 'local',
+            'path'       => storage_path('files'),
+            // 'visibility' => 'public',
+            // 'eventable'  => true,
+            // 'cache'      => 'foo'
+        ],
+
+        'null' => [
+            'driver'    => 'null',
+            // 'eventable' => true,
+            // 'cache'     => 'foo'
+        ],
+
+
+    ],
+
+    /*
+    |--------------------------------------------------------------------------
+    | Flysystem Cache
+    |--------------------------------------------------------------------------
+    |
+    | Here are each of the cache configurations setup for your application.
+    | There are currently two drivers: illuminate and adapter. Examples of
+    | configuration are included. You can of course have multiple connections
+    | per driver as shown.
+    |
+    */
+
+    'cache' => [
+
+        'foo' => [
+            'driver'    => 'illuminate',
+            'connector' => null, // null means use default driver
+            'key'       => 'foo',
+            // 'ttl'       => 300
+        ],
+
+        'bar' => [
+            'driver'    => 'illuminate',
+            'connector' => 'redis', // config/cache.php
+            'key'       => 'bar',
+            'ttl'       => 600,
+        ],
+
+        'adapter' => [
+            'driver'  => 'adapter',
+            'adapter' => 'local', // as defined in connections
+            'file'    => 'flysystem.json',
+            'ttl'     => 600,
+        ],
+
+    ],
+
+];
